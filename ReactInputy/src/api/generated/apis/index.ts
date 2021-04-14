b7b4a1160c66813d04c91713e80da325ea8769be
SzymonKowalski <szymonkowalski878@gmail.com>
2021-04-14 17:43:29 +0200

    Generated api

---
+++ ReactInputy/src/api/generated/apis/index.ts
@@ -0,0 +1,6 @@
+export * from './BloodDonatorApi';
+export * from './BloodStorageApi';
+export * from './BloodTypeApi';
+export * from './DonationApi';
+export * from './ExaminationApi';
+export * from './UserApi';
